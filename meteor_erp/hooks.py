0e494d5d7f207b3f79d1cabe76ed63901f41e4f8
jafarsidik <jeff.sidik@gmail.com>
2025-12-09 19:31:31 +0700

    sd

--- meteor_erp/hooks.py
+++ meteor_erp/hooks.py
@@ -271,7 +271,7 @@ fixtures = [
     # "Product Type",
     # "Product Type",
     "Client Script",
-    "Server Script",
+    #"Server Script",
     # "Role",
     # "Custom DocPerm",
     # "Provinsi",
